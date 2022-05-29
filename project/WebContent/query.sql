6cee658ef0363ef6ce77aa84b5814d12673e05ad
a <87001007+sryh1218@users.noreply.github.com>
2022-05-30 01:03:27 +0900

    SQL 기본 구조

---
+++ project/WebContent/query.sql
@@ -0,0 +1,34 @@
+CREATE table Users(
+	id VARCHAR(15) NOT NULL PRIMARY KEY,
+	name VARCHAR(20) NOT NULL,
+	nick VARCHAR(40) NOT NULL,
+	email VARCHAR(40) NOT NULL UNIQUE,
+	password VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE Notice(
+	id NUMBER NOT NULL PRIMARY KEY,
+	user_id VARCHAR(15) NOT NULL REFERENCES Users(id),
+	title VARCHAR(100),
+	contents VARCHAR(2000),
+	upload_at DATE
+);
+
+CREATE TABLE Community(
+	id NUMBER NOT NULL PRIMARY KEY,
+	user_id VARCHAR(15) NOT NULL REFERENCES Users(id),
+	title VARCHAR(100),
+	category VARCHAR(20) NOT NULL,
+	contents VARCHAR(2000),
+	upload_at DATE
+);
+
+CREATE TABLE Ranking(
+	id NUMBER NOT NULL PRIMARY KEY,
+	player_id VARCHAR(15) NOT NULL REFERENCES Users(id),
+	score NUMBER NOT NULL,
+	play_at DATE
+);
+
+drop table Users;
+
