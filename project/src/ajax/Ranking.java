5dcad8a2801677e896b12227217956cde332dda5
JuSeungKyu <77566626+JuSeungKyu@users.noreply.github.com>
2022-06-10 01:55:52 +0900

    json 연습

--- project/src/ajax/Ranking.java
+++ project/src/ajax/Ranking.java
@@ -7,27 +7,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
 /**
  * Servlet implementation class Ranking
  */
 @WebServlet("/ranking")
 public class Ranking extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+      
     public Ranking() {
         super();
     }
+    
 // 랭킹 불러오기
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		JSONObject jObject = new JSONObject(jsonString);
 	}
 	
 //	랭킹 추가
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		
 	}
 
 }
