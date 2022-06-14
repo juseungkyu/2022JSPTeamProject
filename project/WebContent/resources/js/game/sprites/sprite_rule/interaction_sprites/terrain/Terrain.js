c9125132ff2c584a11c79589ea4c8c50a0c8741c
JuSeungKyu <77566626+JuSeungKyu@users.noreply.github.com>
2022-06-14 23:31:19 +0900

    미처 안 올린 수정 사항 업로드

--- project/WebContent/resources/js/game/sprites/sprite_rule/interaction_sprites/terrain/Terrain.js
+++ project/WebContent/resources/js/game/sprites/sprite_rule/interaction_sprites/terrain/Terrain.js
@@ -2,6 +2,7 @@
 import InteractionSprites from "../InteractionSprites.js";
 import HitBoxType from '/resources/js/constant/HitBoxType.js';
 
+// 지형
 export default class Terrain extends InteractionSprites {
     constructor(x,y,collisionList, animationImageList, size){
         super(x,y,collisionList, HitBoxType.nonpass, animationImageList, size)
