013de64ab9aed1e14e8f99cd2254da487614e5b4
JuSeungKyu <77566626+JuSeungKyu@users.noreply.github.com>
2022-06-14 23:06:35 +0900

    JDBC 원래대로

--- project/src/common/JDBCUtil.java
+++ project/src/common/JDBCUtil.java
@@ -11,12 +11,12 @@ public class JDBCUtil {
 		Connection conn = null;
 		try {
 			
-//			Class.forName("oracle.jdbc.driver.OracleDriver");
-//			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "hr", "hr");
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "hr", "hr");
 			
 //			xe용
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SYSTEM", "1234");
+//			Class.forName("oracle.jdbc.driver.OracleDriver");
+//			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SYSTEM", "1234");
 		} catch (Exception e) {
 			System.out.println("DB 연결 실패");
 			e.printStackTrace();
